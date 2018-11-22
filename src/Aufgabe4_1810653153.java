d0a45cf3ce068ba9a77c63f5ba4d7584cbc45910
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-11-22 11:23:49 +0100

    For, While, Do-While Iteration

---
+++ src/Aufgabe4_1810653153.java
@@ -0,0 +1,29 @@
+public class Aufgabe4_1810653153
+{
+
+    public static void main(String[] args)
+    {
+
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("Der Schleifenzähler ist: " + i);
+        }
+
+        int j = 0;
+
+        while(j <= 10)
+        {
+            System.out.println("Der Schleifenzähler ist: " + j);
+            j++;
+        }
+
+        int k = 0;
+
+        do {
+            System.out.println("Der Schleifenzähler ist: " + k);
+            k++;
+        } while (k <= 10);
+
+    }
+
+}
